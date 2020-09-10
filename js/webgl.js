607ca83d2b3c5ee2d1584236628d13fe0e253b1c
Findu <arttu.hiekkanen@hotmail.com>
2020-09-10 10:19:30 +0300

    github pages not working

--- js/webgl.js
+++ js/webgl.js
@@ -2,7 +2,6 @@ import * as THREE from "../node_modules/three/build/three.module.js";
 
 import {OrbitControls} from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
 
-
 let container;
 let camera;
 let renderer;
